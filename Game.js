cfb51dfa113dc66947b24615e4e07c03a9c598ef
javidVeg <javidx24@icloud.com>
2021-09-08 21:51:11 -0400

    davids changes

--- Game.js
+++ Game.js
@@ -4,18 +4,17 @@
 //show rules
 //person select game type--switch case
 //battle -loop chooseGesture to decide winner until requirement sis met to get to 2 wins
-<<<<<<< HEAD
-switch (playerSelection){
-=======
-//print winner
-//play again?
 
 
-console.log ("Welcome to RPSLS!\nRules:\nRock crushes Scissors\nScissors cuts Paper\nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock")
+class Game {
+    constructor{
+        this.player1 = new Human();
+        this.player2 = null;
+        
+    }
 
 let gameChoice = prompt ("Select your game: 1. Human vs. Computer 2. Human vs. Human")
 switch (menu){
->>>>>>> 8cf67b398c3e907241c141e2a1438525df7e9f89
     case 1:
         text = "Player 1 vs. Player 2"
         break;
@@ -24,26 +23,35 @@ switch (menu){
         break;
 }
 
-<<<<<<< HEAD
 //print winner
 //play again?
 
-// class Game {
-//     constructor{
-//         this.player1 = new Human();
-//         this.player2 = null;
-        
-//     }
-// }
+console.log ("Welcome to RPSLS!\nRules:\nRock crushes Scissors\nScissors cuts Paper\nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock")
 
-console.log(playerSelection)
-=======
+function playAgain (){
+    prompt("Would you like to play again?");
+    return game;
+}
+
 class Game {
     constructor{
         this.player1 = new Human();
         this.player2 = null;
+        
     }
 }
 
-module.exports = Game;
->>>>>>> 8cf67b398c3e907241c141e2a1438525df7e9f89
+console.log(playerSelection)
+
+
+if (Human === "rock"  && player2 === "rock"){
+    console.log("Draw!")
+}else if (player1 === "rock" && player2 === "paper"){
+    console.log("Player 2 wins!")
+    human2.winTotal + 1
+}else if (player1 === "rock" && player2 === "scissors"){
+    console.log("Player 1 wins!")
+    human.winTotal + 1
+}else return
+    
+}
