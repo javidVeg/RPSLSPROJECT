115ce1af31e6c2e89388b8105401df31f74db0e2
amandadomer <amdomer@icloud.com>
2021-09-08 21:04:01 -0500

    added if statements and gameType

--- Game.js
+++ Game.js
@@ -1,44 +1,6 @@
-<<<<<<< HEAD
-
-//game.js
-
-//show rules
-=======
->>>>>>> eb415f4e96a2690d661e19036d87ee2668604c7f
 //person select game type--switch case
 //battle -loop chooseGesture to decide winner until requirement sis met to get to 2 wins
 
-<<<<<<< HEAD
-
-class Game {
-    constructor{
-        this.player1 = new Human();
-        this.player2 = null;
-        
-    }
-
-let gameChoice = prompt ("Select your game: 1. Human vs. Computer 2. Human vs. Human")
-switch (menu){
-    case 1:
-        text = "Player 1 vs. Player 2"
-        break;
-    case 2:
-        text = "Player 1 vs. Computer"
-        break;
-}
-
-//print winner
-//play again?
-
-console.log ("Welcome to RPSLS!\nRules:\nRock crushes Scissors\nScissors cuts Paper\nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock")
-
-function playAgain (){
-    prompt("Would you like to play again?");
-    return game;
-}
-
-=======
->>>>>>> eb415f4e96a2690d661e19036d87ee2668604c7f
 class Game {
     constructor{
         this.player1 = new Human();
@@ -48,7 +10,7 @@ class Game {
     welcome (){
         console.log ("Welcome to RPSLS!\nRules:\nRock crushes Scissors\nScissors cuts Paper\nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock");
     }
-    
+    gameType (){
     let gameChoice = prompt ("Select your game: 1. Human vs. Computer 2. Human vs. Human")
     switch (gameChoice){
         case 1:
@@ -58,6 +20,7 @@ class Game {
             //opens a game with two humans 
         break;
         default:
+      }
     }
     let selection = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
     
@@ -148,16 +111,4 @@ class Game {
 }        
 
 console.log(playerSelection)
-
-
-if (Human === "rock"  && player2 === "rock"){
-    console.log("Draw!")
-}else if (player1 === "rock" && player2 === "paper"){
-    console.log("Player 2 wins!")
-    human2.winTotal + 1
-}else if (player1 === "rock" && player2 === "scissors"){
-    console.log("Player 1 wins!")
-    human.winTotal + 1
-}else return
     
-}
